d12dbf57495a27218baff3ae3c1e9e019b00f69f
makjuho <einjuho7@ajou.ac.kr>
2023-12-20 18:06:26 +0900

    Add views Package Jsp File Delete User properties Configure SecurityFilterChain

--- src/main/java/com/example/SpringSecurity_SignUp_SignIn_Logout/config/SpringSecurityConfig.java
+++ src/main/java/com/example/SpringSecurity_SignUp_SignIn_Logout/config/SpringSecurityConfig.java
@@ -1,12 +1,36 @@
 package com.example.SpringSecurity_SignUp_SignIn_Logout.config;
 
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SpringSecurityConfig {
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        // csrf(), cors() deprecated Method.
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .anyRequest().authenticated()   // 어떤 요청이라도 인증 필요.
+                )
+                .formLogin(login->login
+                        .defaultSuccessUrl("/views/dashboard", true)
+                        .permitAll()
+                )
+                .logout(withDefaults());
+
+        return http.build();
+
+    }
 
 
 }
